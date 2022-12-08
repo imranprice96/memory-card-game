05e6a0853618a0c57d9f0703484d2e5061c01380
imranprice96 <imranprice96@gmail.com>
2022-12-08 17:09:14 +1300

    Source images and add image export file

---
+++ src/images/images.js
@@ -0,0 +1,33 @@
+import anivia from "./anivia.png";
+import galio from "./galio.png";
+import garen from "./garen.png";
+import heimerdinger from "./heimerdinger.png";
+import kaisa from "./kaisa.png";
+import kayn from "./kayn.png";
+import kindred from "./kindred.png";
+import lulu from "./lulu.png";
+import shen from "./shen.png";
+import shyvana from "./shyvana.png";
+import tryndamere from "./tryndamere.png";
+import veigar from "./veigar.png";
+import viego from "./viego.png";
+import yasuo from "./yasuo.png";
+import zed from "./zed.png";
+
+export default {
+	anivia: anivia,
+	galio: galio,
+	garen: garen,
+	heimerdinger: heimerdinger,
+	kaisa: kaisa,
+	kayn: kayn,
+	kindred: kindred,
+	lulu: lulu,
+	shen: shen,
+	shyvana: shyvana,
+	tryndamere: tryndamere,
+	veigar: veigar,
+	viego: viego,
+	yasuo: yasuo,
+	zed: zed,
+};
